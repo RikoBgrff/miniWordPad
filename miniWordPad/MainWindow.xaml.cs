21f7ed35ed47dac68c583adcf0eb5b4303c57b01
Bagirli Arifali Azad <Bagi_mz73@itstep.edu.az>
2021-04-08 16:56:19 +0400

    Add project files.

---
+++ miniWordPad/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace miniWordPad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //MessageBox.Show("Change Toggle Button");
+        }
+
+        private void OpenFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Filter = "Txt Files |*.txt|";
+            fileDialog.DefaultExt = ".txt";
+            Nullable<bool> dialogOk = fileDialog.ShowDialog();
+            if(dialogOk == true)
+            {
+                string fileNames = "";
+                foreach (string item in fileDialog.FileNames)
+                {
+                    fileNames += ";" + item;    
+                }
+                fileNames = fileNames.Substring(1);
+                FilePathTxtBx.Text = fileNames;
+            }
+        }
+    }
+}
