4f530205f39122832785228526dbad8ef12eedcf
RikoBgrff <riko.bagirli777@gmail.com>
2021-04-10 15:24:32 +0400

    Finished!

---
+++ miniWordPad/FileHelper.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniWordPad
+{
+    class FileHelper
+    {
+        public string fileName { get; set; }
+        public string ReadFromFile()
+        {
+            var opener = new OpenFileDialog();
+            opener.Filter = "Txt Files (*.txt)| *.txt";
+
+            if (opener.ShowDialog() != true)
+                return String.Empty;
+
+            fileName = opener.FileName;
+
+            return File.ReadAllText(fileName);
+        }
+
+        public void SaveToFile(string data)
+        {
+            var saver = new SaveFileDialog();
+
+            saver.Filter = "Txt Files (*.txt)| *.txt";
+
+            if (saver.ShowDialog() != true)
+                return;
+
+            fileName = GetFilePath(saver.FileName);
+
+            File.WriteAllText(fileName, data);
+        }
+
+        public void AutoSave(string data)
+        {
+            if (String.IsNullOrWhiteSpace(fileName))
+            {
+                return;
+            }
+            File.WriteAllText(fileName, data);
+        }
+        private string GetFilePath(string filePath)
+        {
+            return filePath.EndsWith(".txt") ? filePath : filePath + ".txt";
+        }
+    }
+
+}
--- miniWordPad/MainWindow.xaml.cs
+++ miniWordPad/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,7 @@ namespace miniWordPad
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly FileHelper _fileHelper = new FileHelper();
         public MainWindow()
         {
             InitializeComponent();
@@ -29,21 +31,56 @@ namespace miniWordPad
 
         private void OpenFileBtn_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Multiselect = true;
-            fileDialog.Filter = "Txt Files |*.txt|";
-            fileDialog.DefaultExt = ".txt";
-            Nullable<bool> dialogOk = fileDialog.ShowDialog();
-            if(dialogOk == true)
-            {
-                string fileNames = "";
-                foreach (string item in fileDialog.FileNames)
-                {
-                    fileNames += ";" + item;    
-                }
-                fileNames = fileNames.Substring(1);
-                FilePathTxtBx.Text = fileNames;
-            }
+            TextTxtBx.Text = _fileHelper.ReadFromFile();
+            FilePathTxtBx.Text = _fileHelper.fileName;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextTxtBx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _fileHelper.SaveToFile(TextTxtBx.Text);
+
+        }
+
+        private void AutoSave_Checked(object sender, RoutedEventArgs e)
+        {
+            _fileHelper.AutoSave(TextTxtBx.Text);
+        }
+
+        private void CutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextTxtBx.Cut();
+        }
+
+        private void CopyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextTxtBx.Copy();
+        }
+
+        private void PasteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextTxtBx.Paste();
+        }
+
+        private void SelectAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextTxtBx.SelectAll();
+        }
+
+        private void SaveBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
+
